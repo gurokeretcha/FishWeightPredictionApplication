06f103f76ad1114dfc9093d3b1ceb5a9ce1adb81
Gurami Keretchashvili <82672441+gurokeretcha@users.noreply.github.com>
2021-12-29 16:13:35 +0400

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ if st.button('Make Prediction'):
         [int(input_species), input_Length1, input_Length2, input_Length3, input_Height, input_Width], 0)
     prediction = best_xgboost_model.predict(inputs)
     print("final pred", np.squeeze(prediction, -1))
-    st.write(f"Your fish weight is: {np.squeeze(prediction, -1)}g")
+    st.write(f"Your fish weight is: {np.squeeze(prediction, -1):.2f}g")
 
     st.write(f"Thank you {st.session_state.name}! I hope you liked it.")
     st.write(f"If you want to see more advanced applications you can follow me on [medium](https://medium.com/@gkeretchashvili)")
